a19e6cf8ef4fc0802f8dfa3eaed3f811b986521e
P Campbell <triciamontreal@gmail.com>
2023-02-15 10:43:03 -0500

    Update 05-return-bool.py
    add a func with if/else

--- solutions/05-return-bool.py
+++ solutions/05-return-bool.py
@@ -8,8 +8,17 @@ def first_smaller2(num1, num2):
   small = num1 < num2
   return small
 
+def first_smaller3(num1, num2):
+  if num1 < num2:
+    small = True
+  else:
+    small = False
+  return small
+
 print("Testing")
-print(smaller(1,2))
-print(smaller(5,2))
-print(smaller2(1,2))
-print(smaller2(5,2))
+print(first_smaller(1,2))
+print(first_smaller(5,2))
+print(first_smaller2(1,2))
+print(first_smaller2(5,2))
+print(first_smaller3(1,2))
+print(first_smaller3(5,2))
