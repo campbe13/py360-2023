b6162b8acbfc4d9ff0bc5fc47b97ef599507aa31
P Campbell <triciamontreal@gmail.com>
2023-03-09 09:19:06 -0500

    Create 07-strings-for.py

---
+++ solutions/07-strings-for.py
@@ -0,0 +1,95 @@
+# strings & for loops, in class exercises
+
+def eight_chars(word):
+  if len(word)>8:
+    return "Too long"
+  elif len(word)<8:
+    return "Too short"
+  else:
+    return "Perfect, thank you for the word starting with " + word[0]
+  
+'''
+two ways to check for a string in a password 
+return True if it's in there, False if not
+'''
+def bad_password(name, password):
+  print("name", name)
+  print("password", password)
+  if name in password:
+    return True 
+  else:
+    return False
+
+def isbad_password(name, password):
+   return (name in password) 
+
+def counte(sentence):
+  count = 0 
+  for letter in sentence:
+    if letter == 'e' or letter == 'E':
+      count += 1
+
+  return count
+
+def reverse_word(word):
+  reversed = ''
+  for letter in word:
+    reversed = letter +reversed
+  return reversed
+
+def secondletters1(word):
+  secondstring = ''
+  ix = 0
+  for letter in word:
+    if ix%2 == 1:
+      secondstring = secondstring+ letter
+    ix += 1
+  return secondstring
+
+
+def main():
+  sentence = input("enter a sentence: ")
+  reverse = reverse_word(sentence)
+  print(f"sentence {sentence} reversed {reverse}")
+  
+  secondstr = secondletters1(sentence)
+  print(f"sentence {sentence} every 2nd letter {secondstr}")
+  
+  num = counte(name)
+  print(f"sentence has {num} e's")
+  
+  name = input("What is your name: ")
+  password = input("What is your password: ")
+
+  if ( isbad_password(name, password)):
+    print("you need a better password")
+  else:
+    print ("password ok")
+
+  name = input("What is your name: ")
+  password = input("What is your password: ")
+  
+  result = isbad_password(name, password)
+  if result:
+    print("you need a better password")
+  else:
+    print ("password ok")
+  input_word = input("enter an 8 letter word: ")
+  print(eight_chars(input_word))
+
+  result = eight_chars(input("enter an 8 letter word: "))
+  print(result)
+
+  input_word = input("enter an 8 letter word: ")
+  result = eight_chars(input_word)
+  print(result)
+  
+  print(eight_chars(input("enter an 8 letter word")))
+ 
+
+#code the main function below  
+
+# Code below ensures that the function called main is invoked when you run
+if __name__ == "__main__":
+    main()
+
