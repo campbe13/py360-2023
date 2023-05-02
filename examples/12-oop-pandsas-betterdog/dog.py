5c6baa529a5035261630f45eaee642051b4ae776
P Campbell <triciamontreal@gmail.com>
2023-05-02 07:51:13 -0400

    Update dog.py

--- examples/12-oop-pandsas-betterdog/dog.py
+++ examples/12-oop-pandsas-betterdog/dog.py
@@ -1,8 +1,8 @@
 '''
 defines the BetterDog class
 
-Improvement on the original Dog class, for use with dataset
-(no age given)
+Improvement on the original Dog class, for use with a dataset
+(no age given in the dataset)
 '''
 
 class BetterDog:
@@ -12,23 +12,23 @@ class BetterDog:
     self.name = name # str
     self.breed = breed   # str
     self.size = size  #str
-    self.age = age  # int
     self.sex = sex  # str
-    #self.canbreed = breedable # bool
     self.colour = colour  # str
+    self.canbreed = breedable # bool
+    self.age =  age  # int
   # behaviours
   def bark(self):
     if self.size in ['small','medium']:
       return 'yip yip yip'
     elif self.size == 'xlarge':
-      return 'ROWF ROWF ROWF'
+      return 'ROWF ROWF RO`WF'
     else:
       return 'rowf rowf rowf'
   def changeAge(self, newage):
     if newage < 0 or newage > 25:
       return
     self.age = newage
-  #def isBreedable(self):
-  #  return self.canbreed
+  def isBreedable(self):
+    return self.canbreed
   def __str__(self):
-    return (f"Name {self.name} breed {self.breed} {self.size} {self.colour}")
+    return (f"Name {self.name.title()} breed {self.breed.capitalize()} {self.size} {self.colour}")
